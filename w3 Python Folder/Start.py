ad0822712948a15e04cda272a7d9f9fdc1801dbd
oprah14 <leventkingman@gmail.com>
2025-04-03 10:34:18 +0000

    python lıbrary

---
+++ w3 Python Folder/Start.py
@@ -0,0 +1,5 @@
+print("Hello World!!")
+# The print() function prints the specified message to the screen, or other standard output device.
+
+#Whenever you are done in the python command line, you can simply type the following to quit the python command line interface:
+exit()
---
+++ w3 Python Folder/syntax.py
@@ -0,0 +1,7 @@
+if 5 > 2:
+  print("Five is greater than two!") #correct (+)
+
+#  The first example we gave here is a correct spelling, but if it is written as seen below, we will get a syntax error
+
+#if 5 > 2:
+#print("Five is greater than two!") #incorrect (-)
